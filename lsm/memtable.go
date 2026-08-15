7ecc2dbff39ac9f5808a611e4ecd7f3f8ced4b8e
Joaquin M. <joaqu26@proton.me>
2026-08-16 06:04:48 +0800

    refactored methods to take in entry

--- lsm/memtable.go
+++ lsm/memtable.go
@@ -1,20 +1,33 @@
 package lsm
 
 type MemTable struct {
-	data map[string]string
+	data map[string]*Entry
+}
+
+type Entry struct {
+	value   string
+	deleted bool
 }
 
 func (m *MemTable) Set(key, value string) {
-	m.data[key] = value
+	m.data[key] = &Entry{
+		value:   value,
+		deleted: false,
+	}
 }
 
 func (m *MemTable) Get(key string) (string, bool) {
-	value, ok := m.data[key]
-	return value, ok
+	entry, ok := m.data[key]
+	if !ok || entry.deleted {
+		return "", false
+	}
+	return entry.value, true
 }
 
 func (m *MemTable) Delete(key string) {
-	delete(m.data, key)
+	m.data[key] = &Entry{
+		deleted: true,
+	}
 }
 
 func (m *MemTable) Size() int {
